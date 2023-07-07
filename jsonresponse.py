5c0d715459f426af57bc4dea2641f28800d1c82e
preejapradeep <105301257+preejapradeep@users.noreply.github.com>
2023-07-07 09:12:00 +0100

    Add files via upload
    .txt to json format

---
+++ jsonresponse.py
@@ -0,0 +1,18 @@
+import json
+#  load and parse a JSON response 
+# json_text = json.loads(response.text)
+# print(json_text)
+
+# Read the contents of the file
+with open('response.text', 'r') as file:
+    json_data = file.read()
+
+# Parse the JSON data
+json_text = json.loads(json_data)
+
+# Specify the output file path
+output_file_path = 'apioutput.json'
+
+# Write the JSON data to the output file
+with open(output_file_path, 'w') as file:
+    json.dump(json_text, file, indent=4)
